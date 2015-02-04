15efb65f5df55fe9527b7103479ef1ef11c0b04a
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-02-03 20:29:28 -0600

    added board view

--- agora/board/urls.py
+++ agora/board/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-import board.urls
+from views import display_page
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'agora.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'', include(board.urls)),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'', display_page),
 )
